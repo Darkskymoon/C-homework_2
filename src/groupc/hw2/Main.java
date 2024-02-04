09be1793c7b922294d7d00c26e85b7ccb041a036
Ryan Wahle <ryanwahle8@gmail.com>
2024-02-04 16:29:17 -0600

    Edited main to uncomment portion related to readFromFile method

--- src/groupc/hw2/Main.java
+++ src/groupc/hw2/Main.java
@@ -5,11 +5,11 @@ import groupc.hw2.student.StudentManager;
 public class Main {
 
 	public static void main(String[] args) {
-		/*
 		// Instantiate StudentManager, perform operations based on the requirements.
 		StudentManager studentManager = new StudentManager();
 		// Read student data from a file and initialize Student objects.
 		boolean fileReadStatus = studentManager.readFromFile("studentData.txt");
+		/*
 		// Display all students.
 		studentManager.displayStudents();
 		// Search for a student by ID.
